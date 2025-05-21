46096f182f2c1f1b8e0ec2bd6fc2432b6087f36e
tinwan.ng <tinwan.ng@govmind.tech>
2025-05-21 17:28:11 +0200

    chore: place char and skele via rectangles

--- jumping_game.py
+++ jumping_game.py
@@ -28,17 +28,20 @@ text_surface = score_font.render("start adventure!", True, "White")
 
 """
 Rectangle
-- can be placed more precisely than surface
+- can be placed via (topleft, midbottom, etc.) compareed to plain number
 - useful for detecting collisions
+- use rectangle.left, for e.g., to measure its x position
 """
+ground_y = size_y-100
+object_y = ground_y-30
+
 character_surface = pygame.image.load("media/swordwoman.png").convert_alpha()  # run faster than basic convert
 character_surface = pygame.transform.scale(character_surface, (80, 70.4))
+character_rect = character_surface.get_rect(topleft=(100, object_y))  # get_rect for generating the eaxct same size as the surface
 
 skeleton_surface = pygame.image.load("media/skeleton.png").convert_alpha()
 skeleton_surface = pygame.transform.scale(skeleton_surface, (60, 50.4))
-
-move_x = init_move_x = size_x-100
-
+sekleton_rect = skeleton_surface.get_rect(topleft=(size_x, object_y))
 
 while True:
 
@@ -49,16 +52,15 @@ while True:
     
     # static surfaces
     screen.blit(planet_surface, (0,0))
-    screen.blit(ground_surface, (0,size_y-100))
+    screen.blit(ground_surface, (0,ground_y))
     screen.blit(text_surface, (25,25))
-    screen.blit(character_surface, (100,size_y-130))
-
     # moving surfaces
-    if move_x <= 0:
-        move_x = init_move_x
-    else:
-        move_x -= 3
-    screen.blit(skeleton_surface, (move_x,size_y-130))
+    screen.blit(character_surface, character_rect)
+    screen.blit(skeleton_surface, sekleton_rect)
+    
+    sekleton_rect.left -= 3
+    if sekleton_rect.left <= 0: sekleton_rect.left = size_x
+    
     
     pygame.display.update()  # -> would keep the window forever open if wo the above block
     clock.tick(60)  # set max 60 loops per sec
